f80526ad374d1757b7261b58f72280c86e1e1e58
abdulrcs <abdulrcs1@gmail.com>
2022-11-02 15:12:47 +0700

    landing section

--- components/LandingSection.js
+++ components/LandingSection.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Box, Button, Heading, Stack, Text } from '@chakra-ui/react';
 
 const LandingSection = () => {
-  return <Heading>Landing Section</Heading>;
+  return (
+   <Box>
+    <Heading>Shoespedia</Heading>
+    <Text mt="20px">Consignment store sepatu nasional yang menyediakan informasi penjualan yang dapat di akses di seluruh indonesa dengan cepat, mudah, dan dapat di percaya</Text>
+    <Button mt="20px" bg="#D9D9D9" borderRadius="100px">Belanja</Button>
+   </Box> 
+  )
 };
 
 export default LandingSection;
