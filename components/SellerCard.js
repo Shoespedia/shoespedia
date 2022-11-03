89cb3d0f2bb928103d443514815b963dab2c6c9e
Rizqi Black <90085130+RizqiBlack19@users.noreply.github.com>
2022-11-03 11:44:39 +0700

    Add SellerCard

---
+++ components/SellerCard.js
@@ -0,0 +1,16 @@
+import { Box, Button, Heading, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const SellerCard = ({ img, title, text, button }) => {
+  return (
+    <Box bg="#F9F9F9" px="32px" py="56px" w="350px">
+      <Image src={img} />
+      <Heading fontSize="20px" mt="8px">{title}</Heading>
+      <Text fontSize='14px' colortext="#666A73" mt="12px" > {text}</Text>
+      <Button size="sm" bg="#D9D9D9" borderRadius="100px" mt="8px">{button}</Button>
+    </Box>
+  )
+}
+
+export default SellerCard;
+
